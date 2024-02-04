2501f2390a5223f02b476e8ad2bde72e676e1230
Ala Trojette <ala.trojette35@gmail.com>
2024-02-04 15:49:23 +0100

    api working +fixed bugs

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import Home from './screen/home.screen';
-
+import { StyleSheet, View } from "react-native";
+import MeteoView from "./sceen/home.sceen.jsx";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import FavoriteScreen from "./sceen/favoris.screen.jsx";
+import storage from "./Storage";
+const Drawer = createDrawerNavigator();
+storage.load({ key: "favorties" }).catch(() => {
+  storage.save({ key: "favorites", data: { value: [] } });
+});
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Home/>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="SkyView">
+        <Drawer.Screen name="SkyView" component={MeteoView} />
+        <Drawer.Screen name="Favorites" component={FavoriteScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: 150
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: 150,
   },
 });
